e830bcc1b5fe3848e9f6fd4af2a82ab47e2d5db6
pietro-sanjines-coderoad-com <pietro.sanjines@mojix.com>
2018-04-02 18:10:55 -0400

    adding modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ReadersComponent } from './readers/readers.component';
+import { ManuscriptComponent } from './manuscript/manuscript.component';
+import { EditorsComponent } from './editors/editors.component';
+import { Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'editors', component: EditorsComponent },
+  { path: 'readers', component: ReadersComponent },
+  { path: 'manuscript', component: ManuscriptComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ReadersComponent,
+    ManuscriptComponent,
+    EditorsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/manuscript/manuscript.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manuscript',
+  templateUrl: './manuscript.component.html',
+  styleUrls: ['./manuscript.component.css']
+})
+export class ManuscriptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
